d3d430a5b393d2b5e661090c8ad7f5f4b07e383f
Guillaume <anakhon@gmail.com>
2016-07-15 19:53:38 +0200

    Thx alpine you don't know function...

--- start.sh
+++ start.sh
@@ -15,35 +15,25 @@ then
     adduser -DS -s /bin/false -u 1000 -G murmur murmur
 fi
 
-# set prop function
-function setProp {
-  local prop=$1
-  local var=$2
-  if [ -n "$var" ]; then
-    echo "Setting $prop to $var"
-    sed -i "/$prop\s*=/ c $prop=$var" $TMP_INI
-  fi
-}
-
 # if no murmur ini has been given / already set, set values and move it
 if [[ ! -f "$FINAL_INI" ]]
 then
-    setProp "database" "$DATABASE"
-    setProp "host" "$HOST"
-    setProp "users" "$USER_COUNT"
-    setProp "serverpassword" "$SERVER_PASSWORD"
-    setProp "welcometext" "$WELCOME_TEXT"
-    setProp "registerName" "$SERVER_NAME"
+    set -i "/database\s*=/ c database=$DATABASE" $TMP_INI
+    set -i "/host\s*=/ c host=$HOST" $TMP_INI
+    set -i "/users\s*=/ c users=$USER_COUNT" $TMP_INI
+    set -i "/serverpassword\s*=/ c serverpassword=$SERVER_PASSWORD" $TMP_INI
+    set -i "/welcometext\s*=/ c welcometext=$WELCOME_TEXT" $TMP_INI
+    set -i "/registerName\s*=/ c registerName=$SERVER_NAME" $TMP_INI
 
     if [[ ! -z "$DB_DRIVER" ]]
     then
-        setProp "dbDriver" "$DB_DRIVER"
-        setProp "dbUsername" "$DB_USERNAME"
-        setProp "dbPassword" "$DB_PASSWORD"
-        setProp "dbHost" "$DB_HOST"
-        setProp "dbPort" "$DB_PORT"
-        setProp "dbPrefix" "$DB_PREFIX"
-        setProp "dbOpts" "$DB_OPTS"
+        sed -i "/dbDriver\s*=/ c dbDriver=$DB_DRIVER" $TMP_INI
+        sed -i "/dbUsername\s*=/ c dbUsername=$DB_USERNAME" $TMP_INI
+        sed -i "/dbPassword\s*=/ c dbPassword=$DB_PASSWORD" $TMP_INI
+        sed -i "/dbHost\s*=/ c dbHost=$DB_HOST" $TMP_INI
+        sed -i "/dbPort\s*=/ c dbPort=$DB_PORT" $TMP_INI
+        sed -i "/dbPrefix\s*=/ c dbPrefix=$DB_PREFIX" $TMP_INI
+        sed -i "/dbOpts\s*=/ c dbOpts=$DB_OPTS" $TMP_INI
     fi
 
     mv -f /tmp/murmur.ini /etc/murmur/murmur.ini
